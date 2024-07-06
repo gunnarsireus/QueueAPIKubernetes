d7aa7141959bc90916d9ecbeb0a0dd05cca59beb
Gunnar Siréus <gunnar.sireus@gmail.com>
2024-07-06 11:34:55 +0200

    Dashboard working

---
+++ CronJobManager/Filter/AuthorizationFilter.cs
@@ -0,0 +1,18 @@
+﻿using Hangfire.Dashboard;
+
+namespace CronJobManager.Filter
+{
+    public class AuthorizationFilter: IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            // Allow all users to see the dashboard (not recommended for production)
+            return true;
+
+            // Implement your custom logic here
+            // Example: Only allow local requests
+            // var httpContext = context.GetHttpContext();
+            // return httpContext.User.Identity.IsAuthenticated && httpContext.Connection.RemoteIpAddress.Equals(IPAddress.Loopback);
+        }
+    }
+}
--- CronJobManager/Program.cs
+++ CronJobManager/Program.cs
@@ -1,27 +1,26 @@
+using CronJobManager.Filter;
 using CronJobManager.Services.Interfaces;
 using CronJobManager.Services.Models;
 using CronJobManager.Services.Services;
+using CronJobManager.Services.Settings;
 using Hangfire;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RestSharp;
+using System.Globalization;
 using System.IO;
 using System.Reflection;
-using CronJobManager.Services.Settings;
-using System.Globalization;
-using Microsoft.AspNetCore.Localization;
-using System.Collections.Generic;
 
 namespace CronJobManager
 {
-    public static class Program
+    public class Program
     {
-
         public static void Main(string[] args)
         {
             Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
@@ -30,49 +29,55 @@ namespace CronJobManager
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
-                {
-                    IConfiguration configuration = hostContext.Configuration;
-                    services.AddSingleton(configuration.GetSection("Resources").Get<ServiceSettings>());
-                    services.AddSingleton(configuration.GetSection("Jobs").Get<JobSettings>());
-                    services.AddSingleton<IMemoryCache>(new MemoryCache(new MemoryCacheOptions { SizeLimit = 1024 }));
-                    services.AddLogging(builder =>
-                    {
-                        builder.AddConsole();
-                        // Add other logging providers as necessary
-                    });
-
-                    var connectionString = configuration.GetConnectionString("CronJobManager");
-                    services.AddHangfire(c => c.UseSqlServerStorage(connectionString));
-                    services.AddHangfireServer();
-                    services.AddSingleton<IRestClientFactory, RestClientFactory>();
-                    services.AddTransient<IJobService, JobService>();
-                    services.AddTransient<IJobManager, JobManager>();
-                    services.AddHostedService<CronJobManagerService>();
-
-                    services.AddControllers();
-                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.Configure(app =>
+                    webBuilder.ConfigureServices((context, services) =>
+                    {
+                        IConfiguration configuration = context.Configuration;
+
+                        services.AddSingleton(configuration.GetSection("Resources").Get<ServiceSettings>());
+                        services.AddSingleton(configuration.GetSection("Jobs").Get<JobSettings>());
+                        services.AddSingleton<IMemoryCache>(new MemoryCache(new MemoryCacheOptions { SizeLimit = 1024 }));
+                        services.AddLogging(builder =>
+                        {
+                            builder.AddConsole();
+                            // Add other logging providers as necessary
+                        });
+
+                        var connectionString = configuration.GetConnectionString("CronJobManager");
+                        services.AddHangfire(config => config.UseSqlServerStorage(connectionString));
+                        services.AddHangfireServer();
+                        services.AddSingleton<IRestClientFactory, RestClientFactory>();
+                        services.AddTransient<IJobService, JobService>();
+                        services.AddTransient<IJobManager, JobManager>();
+                        services.AddHostedService<CronJobManagerService>();
+
+                        services.AddControllers();
+                    })
+                    .Configure((context, app) =>
                     {
                         var defaultDateCulture = "en-US";
                         var ci = new CultureInfo(defaultDateCulture);
 
-                        app.UseRequestLocalization(new RequestLocalizationOptions
+                        var localizationOptions = new RequestLocalizationOptions
                         {
                             DefaultRequestCulture = new RequestCulture(ci),
                             SupportedCultures = [ci],
                             SupportedUICultures = [ci]
+                        };
+
+                        app.UseRequestLocalization(localizationOptions);
+
+                        app.UseHangfireDashboard("/hangfire", new DashboardOptions
+                        {
+                            Authorization = [new AuthorizationFilter()]
                         });
 
                         app.UseRouting();
-                        app.UseHangfireDashboard(); // Add this line to use the Hangfire Dashboard on localhost:5000/hangfire
 
                         app.UseEndpoints(endpoints =>
                         {
                             endpoints.MapHangfireDashboard();
-                            endpoints.MapControllers();
                         });
                     });
                 });
